05ae5b46c881a35752dd077d53f931c9723e1678
Daria <ekgardtdania@gmail.com>
2024-09-17 17:38:07 +0200

    Исправлен баг с проверкой диагоналей, Убран лишний if в функции calcWinner

--- src/board.js
+++ src/board.js
@@ -29,45 +29,31 @@ export class Board {
 
     calcWinner() {
         for (let i = 0; i < 3; i++){
+
             //горизонтальные линии
             if (this.cells[i][0] !== "" && this.cells[i][0] == this.cells[i][1] && this.cells[i][1] == this.cells[i][2]){
-                if (this.cells[i][0] == "x"){
-                    this.winner = "Вы победили!!!";
-                    return this.winner;
-                }
-                this.winner = "Победил бот!";
-                return this.winner;
-            }
-            //вертикальные линии
-            if (this.cells[0][i] !== "" && this.cells[0][i] == this.cells[1][i] && this.cells[1][i] == this.cells[2][i]){
-                if (this.cells[0][i] == "x"){
-                    this.winner = "Вы победили!!!";
-                    return this.winner;
-                }
-                this.winner = "Победил бот!"
-                return this.winner;
-            }
-            //диагональ
-            if (this.cells[0][0] !== "" && this.cells[0][0] == this.cells[1][1] && this.cells[1][1] == this.cells[2][1] ){
-                if (this.cells[0][0] == "x"){
-                    this.winner = "Вы победили!!!";
-                    return this.winner;
-                }
-                this.winner = "Победил бот!"
+                this.winner = this.cells[i][0];
                 return this.winner;
             }
 
-            if (this.cells[0][0] !== "" && this.cells[0][2] == this.cells[1][1] && this.cells[1][1] == this.cells[2][0]){
-                if (this.cells[0][0] == "x"){
-                    this.winner = "Вы победили!!!";
-                    return this.winner;
-                }
-                this.winner = "Победил бот!"
+            //вертикальные линии
+            if (this.cells[0][i] !== "" && this.cells[0][i] == this.cells[1][i] && this.cells[1][i] == this.cells[2][i]){
+                this.winner = this.cells[0][i];
+                return this.winner;
+            }
+            //диагональ
+            if (this.cells[0][0] !== "" && this.cells[0][0] == this.cells[1][1] && this.cells[1][1] == this.cells[2][2] ){
+                this.winner = this.cells[0][0];
+                return this.winner;
+            }
+
+            if (this.cells[0][2] !== "" && this.cells[0][2] == this.cells[1][1] && this.cells[1][1] == this.cells[2][0]){
+                this.winner = this.cells[0][2];
                 return this.winner;
             }
             //ничья
             if (this.cells.flat().every(cell => cell !== "")){
-                this.winner = "Ничья"
+                this.winner = "-"
             }
 
         }
